d86113e6e38f6e2a9d5b7e0895c343c31c5b3daa
R4kun75 <vitor_dalsasso@edu.univali.br>
2026-05-19 16:47:54 -0300

    Setup: Migrated to Modern GUI 1.0 and fixed Gradle setup

--- src/main/java/com/example/addoneconomics/InterfaceEconomics.java
+++ src/main/java/com/example/addoneconomics/InterfaceEconomics.java
@@ -1,20 +1,25 @@
 package com.example.addoneconomics;
 
-import com.dtmpro621.moderngui.screen.ModernScreen;
+
+import com.dtmpro621.moderngui.client.gui.layout.LayoutContext;
+import com.dtmpro621.moderngui.client.gui.layout.UiScale;
+import com.dtmpro621.moderngui.client.gui.layout.VBox;
+import com.dtmpro621.moderngui.client.gui.screen.ModernScreen;
+import com.dtmpro621.moderngui.client.gui.theme.Themes;
+import com.dtmpro621.moderngui.client.gui.widget.Widget;
 import net.minecraft.network.chat.Component;
+
 
 public class InterfaceEconomics extends ModernScreen {
 
-    // O construtor define o título da tela
     public InterfaceEconomics() {
-        super(Component.literal("Menu do Meu Addon"));
+        // Passamos o título e os padrões de tema/escala.
+        // Se Theme ou UiScale ficarem vermelhos, clique neles e dê Alt + Enter para importar!
+        super(Component.literal("Menu do Meu Addon"), Themes.MODERN, UiScale.DEFAULT_AUTO);
     }
 
-    // Este método é chamado quando a tela abre.
-    // É o "onCreate" da sua interface.
     @Override
-    protected void init() {
-        super.init();
-        var root = this.getRootContainer();
+    protected Widget buildRoot(LayoutContext layoutContext) {
+        return null;
     }
 }
